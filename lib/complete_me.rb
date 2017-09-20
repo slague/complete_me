4e68b4615e730bcb33712cc0ea724fada5a630d5
stovermc <stovermc@gmail.com>
2017-09-20 15:04:49 -0600

    add select
    Select and weighted select working properly

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -34,7 +34,7 @@ class CompleteMe
       word = word.delete(word[0])
     end
     final_array = CompleteMe.begin_suggestion(base_word, node)
-    format_display(final_array)
+    format_display(base_word, final_array)
   end
 
   def self.begin_suggestion(word, node)
@@ -43,9 +43,9 @@ class CompleteMe
     return results if node.nil? #This is for when we populate without a full dictionary. It will catch any letters that have no associated word, for example, if we don't put in any z words, and suggest z.
 
     if node.children.empty?
-      results << [word, node.weight]
+      results << [word, node.weights]
     else node.children
-      results << [word, node.weight] if node.end_of_word
+      results << [word, node.weights] if node.end_of_word
 
       node.children.keys.each do | child |
         results << CompleteMe.begin_suggestion((word + child), node.children[child])
@@ -54,42 +54,48 @@ class CompleteMe
       results.flatten
   end
 
-  def format_display(arr)
-    count = 0
-    num = arr.length/2
-    display = []
-    num.times do |a|
-      display << [arr[count], arr[count+1]]
-      count +=2
-    end
-    build_hash(display)
+  def format_display(word, arr)
+    display = arr.each_slice(2).to_a
+    build_hash(word, display)
   end
 
-  def build_hash(arr)
+  def build_hash(word, arr)
     hash = {}
     arr.each do |a|
       hash[a[0]]= a[1]
     end
-    sort_hash(hash)
+    flatten_hash(word, hash)
   end
 
-  def sort_hash(hash)
-    ordered = hash.sort_by{ |k, v| v }.reverse.to_h
+  def flatten_hash(word, hash)
+    flattened_hash = {}
+    hash.each do |k, v|
+      v[word] ? flattened_hash[k] = v[word] : flattened_hash[k] = 0
+    end
+    sort_hash(flattened_hash)
+  end
+
+  def sort_hash(flattened_hash)
+    ordered = flattened_hash.sort_by { |k, v| v }.reverse.to_h
     ordered.keys
   end
 
-  def select(fragment, full_word)
-
-
+  def select(fragment, full_word, node=root)
+    base_word = full_word
+    until full_word.empty?
+      node = node.children[full_word[0]]
+      full_word = full_word.delete(full_word[0])
+      node
+    end
+    node.weights[fragment] ? node.weights[fragment] += 1 : node.weights[fragment] = 1
   end
-
 end
 
 
-# cm = CompleteMe.new
+cm = CompleteMe.new
 # cm.insert('sing')
-# cm.insert('some')
-# cm.insert('something')
+cm.insert('some')
+cm.insert('something')
 # cm.insert('south')
 # cm.insert('soup')
 # cm.suggest('s')
@@ -104,6 +110,6 @@ end
 # # cm.insert("cloud")
 # # cm.insert("mountain")
 # # cm.count
-# cm.suggest('ca')
+cm.select('so', 'some')
 # cm.suggest('c')
 # cm.suggest('z')
